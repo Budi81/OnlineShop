38c890857ad14216d171b7fdc07cca5acf38390a
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-11-07 11:51:35 +0100

    Add tests

---
+++ OnlineShopTests/GetCustomerResultTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OnlineShop.BusinessLayer;
+
+namespace OnlineShopTests
+{
+    [TestFixture]
+    public class GetCustomerResultTests
+    {
+        [Test]
+        public void FoundEntryTest()
+        {
+            var customer = new Customer(1, "Jan", "Kowalski", "Miodowa", "jan@kowalski.pl", "hashed");
+            var getCustomerResult = new GetCustomerResult(customer);
+            var result = getCustomerResult.FoundEntry;
+            Assert.AreEqual(customer, result);
+        }
+
+        [Test]
+        public void FoundEntryTestFail()
+        {
+            var exception = new InvalidOperationException("Wrong login or password!");
+            var getCustomerResult = new GetCustomerResult();
+            var result = getCustomerResult.FoundEntry;
+            Assert.AreEqual(exception, result);
+        }
+    }
+}
